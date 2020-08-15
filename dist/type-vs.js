08e8c37be470f9a393b571ba839c5fb05ff6d89e
Italox <italoaurelior@gmail.com>
2020-08-15 17:33:59 -0300

    :see_no_evil: Adjust gitignore

--- dist/type-vs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-[1, "2"];
-// type JqueryT = { b: string }; <== Error, declare only one "type" with same name
-/*
-Better for most of times
-
-Start with type and change to interface if needed
-*/
