b4fc42aa7eb80bcbb0cf4133346be3c32d0f1e89
Italox <italoaurelior@gmail.com>
2020-08-15 16:51:23 -0300

    :sparkles: Generics

---
+++ generics.ts
@@ -0,0 +1,36 @@
+// Some patterns name for generics
+// S => State
+// T => Type
+// K => Key
+// V => Value
+// E => Element
+
+type numberOrString = number | string;
+
+function useState<S extends numberOrString = string>() {
+  let state: S;
+
+  function getState() {
+    return state;
+  }
+
+  function setState(newState: S) {
+    state = newState;
+  }
+
+  return { getState, setState };
+}
+
+const myState = useState<number>();
+
+// The first type definition become default
+myState.setState(1);
+
+console.log(myState.getState());
+
+// myState.setState("World"); <== Error because change the above setted type number
+
+// If not pass the <TYPE> in useState get the default generic = string
+const otherState = useState();
+otherState.setState("Heio");
+console.log(otherState.getState());
